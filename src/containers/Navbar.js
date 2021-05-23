b82ad444422dcbc48dd28807aeef90ec4330942e
Carlos Abarca <cabarca@urbano.com.sv>
2021-05-23 15:58:11 -0600

    cambios a navbar

--- src/containers/Navbar.js
+++ src/containers/Navbar.js
@@ -5,13 +5,19 @@ import {NavLink} from 'react-router-dom'
 
 const Navbar = () => {
     return ( 
-        <nav className='bg-blue-900 py-10 flex justify-center '>
+        <nav className='bg-blue-900 py-8 flex justify-between '>
+          <div className='w-1/2 mx-10'>
+      <h1 className='text-yellow-400 text-2xl'>PokeTest</h1>
+          </div>
+          <div className='w-1/2 flex flew-row justify-end'>
+
         <NavLink
-          className={'text-4xl text-gray-300 pr-8'} 
+          className='text-2xl text-gray-300 pr-8' 
           to={"/"}>Home</NavLink>
         <NavLink
-          className={'text-4xl text-gray-300 pr-8'} 
+          className='text-2xl text-gray-300 pr-8' 
           to={"/PokeTeam"}>PokeTeam</NavLink>
+          </div>
       </nav>
      );
 }
